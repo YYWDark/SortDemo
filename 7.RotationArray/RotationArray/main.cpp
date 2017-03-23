2eac39c1b0b38da69655e1711e9043149a83ca41
wangyayun0629@163.com <wangyayun0629@163.com>
2017-03-23 10:49:46 +0800

    motification sort

--- 7.RotationArray/RotationArray/main.cpp
+++ 7.RotationArray/RotationArray/main.cpp
@@ -8,17 +8,31 @@
 
 #include <iostream>
 
+int sequenceSort(int *numbers, int pre, int last) {
+    int value = numbers[pre];
+    for (int index = pre + 1; index < last; index ++) {
+        if (value > numbers[index]) return  numbers[index];
+    }
+    return value;
+}
+
 int min(int *numbers ,int lenght) {
     if (numbers == NULL || lenght < 0) return -1;
     int pre = 0;
     int last = lenght - 1;
     int indexMid = pre;
+    if(numbers[pre] < numbers[last]) return numbers[pre];
     while (numbers[pre] >= numbers[last]) {
         if (last - pre == 1) {
             indexMid = last;
             break;
         }
         indexMid = (pre + last)/2;
+        
+        if (numbers[pre] == numbers[indexMid] && numbers[last] == numbers[indexMid]) {
+            return  sequenceSort(numbers, pre, last);
+        }
+        
         if (numbers[indexMid] >= numbers[pre]) {
             pre = indexMid;
         }else if (numbers[indexMid] <= numbers[last]) {
@@ -34,9 +48,9 @@ int main(int argc, const char * argv[]) {
 //    int in[8]  = {1,2,3,4,5,6,7,8};
 //    int in[6] = {4,5,6,1,2,3};
 //    int in[7] = {4,5,6,7,1,2,3};
-    int in[6] = {5,6,1,2,3,4};
-
-    int b =  min(in,6);
+//    int in[6] = {5,6,1,2,3,4};
+    int in[5] = {1,0,1,1,1};
+    int b =  min(in,5);
     
     return 0;
 }
